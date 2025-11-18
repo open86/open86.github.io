39e2bdac7340d054f70788935210219205c36e3c
open86 <open86@users.noreply.github.com>
2025-11-18 10:28:14 +0000

    deploy: open86/open86.github.io@d745d487b0ce078b541bef83e11c15cbf0481f02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/这是另一篇新的博文/","wenzhang/在哪里/","wenzhang/在这里/","wenzhang/hello-world/","wenzhang/风格哥哥/","wenzhang/这是一篇新的博文000/","wenzhang/这是一篇新的文章/"];function toRandomPost(){
+var posts=["wenzhang/这是另一篇新的博文/","wenzhang/hello-world/","wenzhang/风格哥哥/","wenzhang/这是一篇新的博文000/","wenzhang/这是一篇新的文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
