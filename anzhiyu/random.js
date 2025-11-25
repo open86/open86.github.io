43587f7bf28ecbd3aebc7a45c2533aab31f1fb78
open86 <yuanguo024@gmail.com>
2025-11-25 13:22:42 +0800

    Site updated: 2025-11-25 13:22:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/16/这是一篇新的博文66666/","2025/11/16/你好啊/","2025/11/16/hello-world/","2025/11/16/这是一篇新的博文8888/","2025/11/16/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/11/25/这是一片新的博文/","2025/11/17/反倒是/","2025/11/16/你好啊/","2025/11/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
